99556fa722c8910f589245430659094b236d2cb8
jomariepolanco <jomariepolanco@gmail.com>
2021-01-10 14:47:03 -0500

    sidebar rending title of tracks

--- client/src/containers/Sidebar.tsx
+++ client/src/containers/Sidebar.tsx
@@ -13,6 +13,8 @@ import { List } from '@material-ui/core'
 import ProducerSearch from '../components/ProducerSearch'
 import GenreSearch from '../components/GenreSearch'
 import { Producer } from '../store/producer/models/Producer'
+import { boundAudios } from '../store/audio/AudioAction'
+import { Audio } from '../store/audio/models/Audio'
 
 
 
@@ -21,11 +23,13 @@ interface Props {}
 interface LinkStateProps {
     genres: Genre[],
     producers: Producer[];
+    audios: Audio[];
 }
 
 interface LinkDispatchProps {
     boundGenres: () => void;
     boundProducers: () => void;
+    boundAudios: () => void;
 }
 
 type LinkProps = LinkStateProps & LinkDispatchProps & Props
@@ -40,17 +44,34 @@ class Sidebar extends Component<LinkProps>{
     componentDidMount(){
         this.props.boundGenres()
         this.props.boundProducers()
+        this.props.boundAudios()
     }
 
     renderGenres = () => {
+        if (this.state.genreInput.length === 0){
+            return null
+        } else {
             const filterGenres = [...this.props.genres].filter(genre => genre.name.toLowerCase().includes(this.state.genreInput.toLowerCase()))
             return filterGenres.map(genre => <GenreCard key={genre.id} name={genre.name} />)
+        }
     }
 
     renderProducers = () => {
-        const filterProducers = [...this.props.producers].filter(producer => producer.name.toLowerCase().includes(this.state.producerInput.toLowerCase()) || producer.username.toLowerCase().includes(this.state.producerInput.toLowerCase()))
-        return filterProducers.map(producer => <ProducerCard key={producer.id} name={producer.name} username={producer.username} />)
+        if (this.state.producerInput.length === 0) {
+            return null
+        } else {
+            const filterProducers = [...this.props.producers].filter(producer => producer.name.toLowerCase().includes(this.state.producerInput.toLowerCase()) || producer.username.toLowerCase().includes(this.state.producerInput.toLowerCase()))
+            return filterProducers.map(producer => <ProducerCard key={producer.id} name={producer.name} username={producer.username} />)
+        }
 
+    }
+
+    renderAudios = () => {
+        return [...this.props.audios].map(audio => <><div id={audio.title} onClick={this.renderAudioCard}key={audio.id}>{audio.title}</div><br/><br/></>)
+    }
+
+    renderAudioCard = (e: any) => {
+        const audio = [...this.props.audios].find(audio => audio.title === e.target.id)
     }
 
     onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
@@ -62,10 +83,10 @@ class Sidebar extends Component<LinkProps>{
             <aside>
                 <GenreSearch genreInput={this.state.genreInput} genres={this.props.genres} genreChangeHandler={this.onChangeHandler} />
                 <ProducerSearch producerInput={this.state.producerInput} producers={this.props.producers} producerChangeHandler={this.onChangeHandler}/>
-               <List component="nav" style={{maxWidth: '15%'}}>
+               <List component="nav" style={{ textAlign: 'center'}}>
                     {this.renderGenres()}
                     {this.renderProducers()}
-
+                    {this.renderAudios()}
                </List>
 
                
@@ -76,11 +97,13 @@ class Sidebar extends Component<LinkProps>{
 
 const msp = (state: AppState): LinkStateProps => ({
     genres: state.genreReducer.genres,
-    producers: state.producerReducer.producers
+    producers: state.producerReducer.producers,
+    audios: state.audioReducer.audios
 })
 
 const mdp = (dispatch: ThunkDispatch<AppState, {}, AppActions>) => ({
     boundGenres: bindActionCreators(boundGenres, dispatch),
-    boundProducers: bindActionCreators(boundProducers, dispatch)
+    boundProducers: bindActionCreators(boundProducers, dispatch),
+    boundAudios: bindActionCreators(boundAudios, dispatch)
 })
 export default connect(msp, mdp)(Sidebar)
