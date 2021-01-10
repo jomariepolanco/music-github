36713a8fd2b929e2e413be7c41a7d8622f276341
jomariepolanco <jomariepolanco@gmail.com>
2021-01-10 00:23:47 -0500

    Successfully rendered contributions

--- client/src/components/ContributionCard.tsx
+++ client/src/components/ContributionCard.tsx
@@ -1,13 +1,26 @@
 import { Card, CardContent } from '@material-ui/core'
 import React, { Component } from 'react'
 
-export default class ContributionCard extends Component {
+interface Props {
+    comment: string;
+    track: string;
+    isAccepted: boolean;
+    date: Date;
+}
+
+export default class ContributionCard extends Component<Props>{
     render() {
         return (
             <div>
                 <Card variant="outlined">
                     <CardContent>
-                        contribution card
+                        <h6>{this.props.date}</h6>
+                        <audio controls>
+                            <source src={this.props.track}/>    
+                        </audio>
+                        <p>{this.props.comment}</p>
+                        <button>Accept</button>
+                        <button>Deny</button>
                     </CardContent>
                 </Card>
             </div>
--- client/src/containers/ContributionsContainer.tsx
+++ client/src/containers/ContributionsContainer.tsx
@@ -1,13 +1,51 @@
 import { Container } from '@material-ui/core'
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { ThunkDispatch } from 'redux-thunk'
 import ContributionCard from '../components/ContributionCard'
+import { boundContributions } from '../store/contribution/ContributionAction'
+import { Contribution } from '../store/contribution/models/Contribution'
+import { AppActions } from '../store/models/actions'
+import { AppState } from '../store/rootStore'
 
-export default class ContributionsContainer extends Component {
+interface Props {}
+
+interface LinkStateProps {
+    contributions: Contribution[]
+}
+
+interface LinkDispatchProps {
+    boundContributions: () => void;
+}
+
+type LinkProps = LinkStateProps & LinkDispatchProps & Props
+
+class ContributionsContainer extends Component<LinkProps>{
+
+    componentDidMount(){
+        this.props.boundContributions()
+    }
+
+
+    renderContributions = () => {
+        return [...this.props.contributions].map(c => <ContributionCard key={c.id} comment={c.comment} track={c.track} isAccepted={c.isAccepted} date={c.date}/>)
+    }
     render() {
         return (
             <Container fixed>
-                <ContributionCard />
+                {this.renderContributions()}
             </Container>
         )
     }
 }
+
+const msp = (state: AppState): LinkStateProps => ({
+    contributions: state.contributionReducer.contributions
+})
+
+const mdp = (dispatch: ThunkDispatch<AppState, {}, AppActions>) => ({
+    boundContributions: bindActionCreators(boundContributions, dispatch)
+})
+
+export default connect(msp, mdp)(ContributionsContainer)
