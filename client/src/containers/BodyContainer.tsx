13743c7fe7924b98d98eb6a8042b1fe6fed56e90
jomariepolanco <jomariepolanco@gmail.com>
2021-01-10 18:40:38 -0500

    Refactored layout in Body Container

--- client/src/containers/BodyContainer.tsx
+++ client/src/containers/BodyContainer.tsx
@@ -1,14 +1,30 @@
 import React, { Component } from 'react'
 import ContributionsContainer from './ContributionsContainer'
 import AudioContainer from './AudioContainer'
+import { createStyles, makeStyles, Paper, Theme } from '@material-ui/core'
 
-export default class BodyContainer extends Component {
-    render() {
-        return (
-            <div>
-                <AudioContainer />
-                <ContributionsContainer />
-            </div>
-        )
+const useStyles = makeStyles((theme: Theme) => 
+  createStyles({
+    paper: {
+      padding: theme.spacing(3),
+      textAlign: 'center',
+      color: theme.palette.text.secondary
     }
+  })
+)
+
+const BodyContainer: React.FC = () => {
+    const classes = useStyles()
+    return (
+        <div>
+            <Paper className={classes.paper}>
+                <AudioContainer />
+            </Paper>
+            <Paper className={classes.paper}>
+                <ContributionsContainer />
+            </Paper>
+        </div>
+    )
 }
+
+export default BodyContainer;
