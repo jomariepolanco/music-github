e840bb8e31fb454f40d4e74d18c519f3268391c1
jomariepolanco <jomariepolanco@gmail.com>
2021-01-11 00:05:38 -0500

    installed react-router and wrapped app in browser router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom'
 import Sidebar from './containers/Sidebar';
 import 'fontsource-roboto'
 import Navbar from './components/Navbar';
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom'
 import './index.css';
 import App from './App';
 import {Provider} from 'react-redux'
@@ -7,9 +8,11 @@ import {store} from './store/rootStore'
 
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
