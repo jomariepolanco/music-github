dfd4de66cee665e9a558dfebda655428bbe1c16d
jomariepolanco <jomariepolanco@gmail.com>
2021-01-10 19:25:31 -0500

    fixed layout of audio player

--- client/src/components/AudioCard.tsx
+++ client/src/components/AudioCard.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent } from '@material-ui/core'
+import { Card, CardContent, Typography } from '@material-ui/core'
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Audio } from '../store/audio/models/Audio'
@@ -12,18 +12,43 @@ interface LinkStateProps {
 
 type LinkProps = LinkStateProps & Props
 
+// const styles = {
+   
+//     details: {
+//       display: 'flex',
+//       flexDirection: 'column',
+//     },
+//     cover: {
+//       width: 151,
+//     },
+//     controls: {
+//       display: 'flex',
+//       alignItems: 'center',
+//       paddingLeft: theme.spacing(1),
+//       paddingBottom: theme.spacing(1),
+//     },
+//     playIcon: {
+//       height: 38,
+//       width: 38,
+//     },
+//   }
+
+
 class AudioCard extends Component<LinkProps>{
-    
     render() {
-        console.log(this.props.audio)
         return (
             <div>
-            <Card variant="outlined">
-                <CardContent>
-                    <h3>{this.props.audio.title}</h3>
-                    <audio controls>
-                        <source src={this.props.audio.track}/>
-                    </audio>
+            <Card variant="outlined" style={{display: "flex"}}>
+                <CardContent style={{flex: '1 0 auto'}}>
+                    <Typography component="h3">
+                        {this.props.audio.title}
+                    </Typography>
+                    <br/>
+                    <div>
+                        <audio controls preload="auto">
+                            <source src={this.props.audio.track}/>
+                        </audio>
+                    </div>
                 </CardContent>
             </Card>
             </div>
